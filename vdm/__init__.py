f027b53e741770900b8684f0679c3e7ba190af2d
rgrp <rufus.pollock@okfn.org>
2008-05-30 11:29:03 +0000

    [vdm/docs]: minor refactoring of 'README' in main docstring.

--- vdm/__init__.py
+++ vdm/__init__.py
@@ -2,12 +2,11 @@
 About
 =====
 
-A versioned domain model framework.
-
-The vdm package allows you to 'version' your domain model in the same way that
-source code version control systems such as subversion allow you version your
-code. In particular, versioned domain model versions a complete model and not
-just individual domain objects (for more on this distinction see below).
+Versioned Domain Model (vdm) is a package which allows you to 'version' your
+domain model in the same way that source code version control systems such as
+subversion allow you version your code. In particular, versioned domain model
+versions a complete model and not just individual domain objects (for more on
+this distinction see below).
 
 At present the package is provided as an extension to SQLAlchemy and SQLObject
 (with an extension to Elixir in progress).
@@ -29,46 +28,12 @@ Authors
 Rufus Pollock <rufus [at] rufuspollock [dot] org>
 
 
-Conceptual Documentation
-========================
+A Full Versioned Domain Model
+=============================
 
-A great starting point is Fowler's *Patterns for things that change with time*:
-
-<http://www.martinfowler.com/ap2/timeNarrative.html>
-
-In particular Temporal Object:
-
-<http://www.martinfowler.com/ap2/temporalObject.html>
-
-Two possible approaches:
-
-  1. (simpler) Versioned domain objects are versioned independently (like a
-     wiki). This is less of a versioned 'domain model' and more of plain
-     versioned domain objects.
-  2. (more complex) Have explicit 'Revision' object and multiple objects can be
-     changed simultaneously in each revision (atomicity). This is a proper
-     versioned *domain model*.
-
-Remark: using the first approach it is:
-
-  * Impossible to support versioning of many-to-many links between versioned
-    domain objects.
-  * It is impossible to change multiple objects 'at once' -- that is as part of
-    one atomic change
-  * More discussions of limitations can be found in this thread [1] as well as
-    in vdm/elixir/README.txt.
-
-[1]:<http://groups.google.com/group/sqlelixir/browse_thread/thread/50aee902ce3555fb/>
-
-The versioned domain model (vdm) package focuses on supporting the second case
-(this obviously includes the first one as a subcase) -- hence the name.
-
-Full Versioned Domain Model
----------------------------
-
-To encapsulate changing multiple objects at once as well as to facilitate
-domain object traversal it is necessary to introduce an explicit 'Revision'
-object to represent a single changeset to the domain model.
+To permit 'atomic' changes involving multiple objects at once as well as to
+facilitate domain object traversal it is necessary to introduce an explicit
+'Revision' object to represent a single changeset to the domain model.
 
 One also needs to introduce the concept of 'State'. This allows us to make
 (some) domain objects stateful, in particular those which are to be versioned
@@ -79,7 +44,7 @@ For each original domain object that comes versioned we end up with 2 domain
 objects:
 
   * The 'continuity': the original domain object.
-  * The 'version/revision' of that domain object.
+  * The 'version/revision': the versions/revisions of that domain object.
 
 Often a user will never need to be concerned (explicitly) with the
 version/revision object as they will just interact with the original domain
@@ -131,15 +96,53 @@ Code in Action
 
 To see some real code in action take a look at::
 
-  vdm/sqlalchemy/demo.py
-  vdm/sqlalchemy/demo_test.py
+    vdm/sqlalchemy/demo.py
+    vdm/sqlalchemy/demo_test.py
 
-  vdm/sqlobject/demo.py
-  vdm/sqlobject/demo_test.py
+    vdm/sqlobject/demo.py
+    vdm/sqlobject/demo_test.py
 
-The code for elixir, which is not yet fully functional, can be found in:
+The code for elixir, which is not yet fully functional, can be found in::
 
-  vdm/elixir/
+    vdm/elixir/
+
+
+General Conceptual Documentation
+================================
+
+A great starting point is Fowler's *Patterns for things that change with time*:
+
+<http://www.martinfowler.com/ap2/timeNarrative.html>
+
+In particular Temporal Object:
+
+<http://www.martinfowler.com/ap2/temporalObject.html>
+
+Two possible approaches:
+
+  1. (simpler) Versioned domain objects are versioned independently (like a
+     wiki). This is less of a versioned 'domain model' and more of plain
+     versioned domain objects.
+  2. (more complex) Have explicit 'Revision' object and multiple objects can be
+     changed simultaneously in each revision (atomicity). This is a proper
+     versioned *domain model*.
+
+Remark: using the first approach it is:
+
+  * Impossible to support versioning of many-to-many links between versioned
+    domain objects.
+  * Impossible to change multiple objects 'at once' -- that is as part of
+    one atomic change
+  * Difficult to support domain model traversal, that is the ability to
+    navigate around the domain model at a particular 'revision'/point-in-time.
+  * More discussions of limitations can be found in this thread [1] as well as
+    in vdm/elixir/README.txt.
+
+[1]:<http://groups.google.com/group/sqlelixir/browse_thread/thread/50aee902ce3555fb/>
+
+The versioned domain model (vdm) package focuses on supporting the second case
+(this obviously includes the first one as a subcase) -- hence the name.
+
 '''
 __version__ = '0.2'
 __description__ = 'A versioned domain model framework.'
